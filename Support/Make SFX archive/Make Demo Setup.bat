db4152cc5b2a0c38f937b5149d9c6fe2cc29fe7b
Pieter van Ginkel <pvginkel@gmail.com>
2012-12-26 06:52:41 +0100

    Fixed demo Make Setup script.

--- Support/Make SFX archive/Make Demo Setup.bat
+++ Support/Make SFX archive/Make Demo Setup.bat
@@ -1,7 +1,7 @@
 @echo off
 
 pushd "%~dp0"
-powershell -file .\MakeSetup.ps1 "Setup.exe" "C:\Projects\NuGetUpdate\Build\Distrib\Packages" "NuGetUpdate.Demo" "NuGet Update Demo"
+powershell -file .\MakeSetup.ps1 "Setup.exe" "http://nuget.org/api/v2" "NuGetUpdate.Demo" "NuGet Update Demo"
 popd
 
 pause
