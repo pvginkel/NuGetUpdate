a965f939b86a38e46866750625611e80008acd45
Pieter van Ginkel <pvginkel@gmail.com>
2017-10-31 10:16:52 +0100

    Fixed publish script.

--- Build/Publish Packages.bat
+++ Build/Publish Packages.bat
@@ -2,11 +2,11 @@
 
 pushd "%~dp0"
 
-set NUGET="%~dp0\..\.nuget\NuGet.exe"
+set NUGET="%~dp0\Support\NuGet.exe"
 
 pushd "%~dp0\Distrib\Packages"
 
-for %%f in (*.nupkg) do %NUGET% push %%f
+for %%f in (*.nupkg) do %NUGET% push -source https://www.nuget.org %%f
 
 pause
 
